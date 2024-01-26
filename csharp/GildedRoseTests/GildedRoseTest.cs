97749aa20eb0288765fddfa4c5e3221c8d0dd79e
cedric meleard <cedric.meleard@n2f.com>
2024-01-26 21:07:41 +0100

    initial setup

---
+++ csharp/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRose;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void Foo()
+    {
+        IList<Item> items = new List<Item> { new() { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose.GildedRose app = new(items);
+        app.UpdateQuality();
+        Assert.Equal("fixme", items[0].Name);
+    }
+}
